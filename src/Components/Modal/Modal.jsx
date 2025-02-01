866a47364817795a3e677c1df006953d262b4cce
SHIJIN ABRAHAM <shijinabraham2003@gmail.com>
2025-02-01 11:24:09 +0530

    Updated Modal

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Modal = ({ children }) => {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <div className="h-fit py-10 w-[30%] z-50 backdrop-blur-3xl bg-[#00011] flex justify-center items-center rounded-4xl drop-shadow-lg">
+      <div className="h-fit max-h-[90vh] py-10 w-128 z-50 backdrop-blur-3xl bg-[#00011] overflow-auto flex justify-center rounded-4xl drop-shadow-lg scrollbar-none">
         <div className="h-fit w-[90%] rounded-4xl flex flex-col gap-8">
           {children}
         </div>
